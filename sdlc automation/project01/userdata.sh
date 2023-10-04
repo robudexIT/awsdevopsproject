c1f9243aa6167c0ee14a887b7c27e464bda10180
robudexIT <103070901+robudexIT@users.noreply.github.com>
2023-10-05 02:23:50 +0800

    Update userdata.sh

--- sdlc automation/project01/userdata.sh
+++ sdlc automation/project01/userdata.sh
@@ -7,9 +7,6 @@
 /bin/python3 -m pip install boto3
 
 
-#Copy the script from s3 bucket to current directory
-/bin/aws s3 cp s3://your-bucket-name/getkey.py  .
-
 #Install requests module
 /bin/python3 -m pip install requests
 
